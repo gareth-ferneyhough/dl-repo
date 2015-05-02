604155de0cd78de8884fa56957e4f655ffe108c9
Gareth Ferneyhough <gareth.ferneyhough@gmail.com>
2015-05-01 18:20:13 -0700

    Downloads file provided by url.

--- app/dl_repo/controllers.py
+++ app/dl_repo/controllers.py
@@ -1,11 +1,20 @@
-# Import flask dependencies
+import requests
 from flask import Blueprint, request, render_template, \
-        flash, g, session, redirect, url_for
-
+        url_for, make_response
 
 dl_repo = Blueprint('dl_repo', __name__, url_prefix='/')
 
-# Set the route and accepted methods
 @dl_repo.route('/', methods=['GET'])
 def index():    
     return render_template("dl_repo/index.html")
+
+@dl_repo.route('download', methods=['GET'])
+def download():
+    url = request.args.get('url')
+    compression = request.args.get('compression')
+        
+    file = requests.get(url)
+    response = make_response(file.content)    
+    response.headers["Content-Disposition"] = "attachment; filename=file.html"
+    return response
+    #return render_template("dl_repo/index.html") 
