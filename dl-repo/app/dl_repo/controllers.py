127ad04078857c9cfc72bb60a6e7737c05b7fbcf
Gareth Ferneyhough <gareth.ferneyhough@gmail.com>
2015-06-10 22:52:44 -0700

    Fixed temporary files location.

--- dl-repo/app/dl_repo/controllers.py
+++ dl-repo/app/dl_repo/controllers.py
@@ -2,6 +2,8 @@ from repository_downloader import download_repository
 from compressor import compress_repository
 from flask import Blueprint, request, render_template, \
         url_for, make_response, send_from_directory, flash, redirect
+from flask import current_app as app
+import os
 
 dl_repo = Blueprint('dl_repo', __name__, url_prefix='/')
 
@@ -11,15 +13,16 @@ def index():
 
 @dl_repo.route('download', methods=['GET'])
 def download():
-    # url = request.args.get('url')
-    # compression = request.args.get('compression')
+    url = request.args.get('url')
+    compression = request.args.get('compression')
+    tmp_dir = os.path.join(app.config['BASE_DIR'], 'tmp')
     
-    # try:
-    #     destination_path = download_repository(url)
-    #     filename = compress_repository(destination_path, compression)
-    # except Exception as e:
-    #     flash(str(e))
-    #     return redirect(url_for('.index'))
-
-    return send_from_directory('tmp', 'git.zip', 
+    try:
+        destination_path = download_repository(url, tmp_dir)
+        filename = compress_repository(destination_path, compression)
+    except Exception as e:
+        flash(str(e))
+        return redirect(url_for('.index'))
+        
+    return send_from_directory(tmp_dir, 'git.zip',
         as_attachment=True, attachment_filename='download.zip')
--- dl-repo/app/dl_repo/repository_downloader.py
+++ dl-repo/app/dl_repo/repository_downloader.py
@@ -6,10 +6,10 @@ def git(*args):
     return subprocess.check_call(['git'] + list(args))
 
 # currently only supports git clone via ssh 
-def download_repository(repository_url):
+def download_repository(repository_url, temp_directory):
 	# this is in no way ideal, as we are deleting
 	# all the repos every time we run.
-	destination_path = 'app/tmp/git'	
+	destination_path = os.path.join(temp_directory, 'git')
 	if os.path.isdir(destination_path):
 		shutil.rmtree(destination_path) 
 	git('clone', repository_url, destination_path)
